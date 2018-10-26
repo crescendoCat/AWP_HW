9e29c7011846ac96ab5eb18cef0f9268a03a2a6b
b03902087 <b03902087@ntu.edu.tw>
2018-10-26 22:25:01 +0800

    fixed internal server error while getting caption

--- caption.php
+++ caption.php
@@ -3,7 +3,7 @@ include('database.php');
 $conn = connectOurtubeDatabase();
 if(isset($_GET['youtubeid'])) {
     $youtubeId = $_GET['youtubeid'];
-    exit(echo getVideoCaption($conn, $youtubeId));
+    exit(getVideoCaption($conn, $youtubeId));
 } else {
     echo 'error: No youtube id!';
     header('Location: index.php');
