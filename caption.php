34f5cdba54096baaa0e58585048e2454e1b397d7
crescendoCat <crescendoCat@c.crescendo.cat@gmail.com>
2018-10-28 22:20:59 +0900

    Update caption.php
    add the preprocess of json string

--- caption.php
+++ caption.php
@@ -1,9 +1,10 @@
-<?
+<?php
 include('database.php');
+include('utility.php');
 $conn = connectOurtubeDatabase();
 if(isset($_GET['youtubeid'])) {
     $youtubeId = $_GET['youtubeid'];
-    exit(getVideoCaption($conn, $youtubeId));
+    exit(jsonPreprocess(getVideoCaption($conn, $youtubeId)));
 } else {
     echo 'error: No youtube id!';
     header('Location: index.php');
