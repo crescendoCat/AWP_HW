cd146da2cb614f35bcc8133aa23a53ca78c584b4
BillChiu-tw <32223761+BillChiu-tw@users.noreply.github.com>
2018-11-17 15:33:44 +0800

    Update youtube-captions.php

--- youtube-captions.php
+++ youtube-captions.php
@@ -1,199 +1,309 @@
 <?php
-require_once 'utility.php';
+$host = 'http://40.121.221.31:8888/';
+$database_thumbnail_folder_path = 'Videos/';
+$apiKey="AIzaSyBPS3aToTVD0SXGNYZpEgAwf43CahHK-58";
+
+class Video{
+	public $videoId;
+	public $title;
+	public $description;
+	public $video_width;
+	public $video_height;
+	public $thumbnail_url;
+	public $thumbnail_width;
+	public $thumbnail_height;
+	public $author_url;
+	public $author_name;
+	public $channelId;
+	// The value is specified in ISO 8601 (YYYY-MM-DDThh:mm:ss.sZ) format.
+	public $publishDate;
+	
+	// param: The value is specified in ISO 8601 (YYYY-MM-DDThh:mm:ss.sZ) format.
+	// Return: standard datetime format in php
+	function convertIso8601toDateTime($iso8601Datetime){
+		 return date('Y-m-d h:i:s', strtotime(substr($iso8601Datetime,0,19)));
+	}	
+}
 
 
+function connectOurtubeDatabase() {
+    return mysqli_connect('40.121.221.31', 'nthuuser', '1qaz@WSX3edc');
+}
 
-$autoRedirect = (isset($_POST['autoRedirect']) && $_POST['autoRedirect']=="Yes") ? true : null;
-$redirectSsl = (isset($_POST['redirectSSL']) && $_POST['redirectSSL']=="Yes") ? true : null;
-$redirectImmediately = (isset($_POST['redirectImmediately']) && $_POST['redirectImmediately']=="Yes") ? true : null;
-
-if(isset($_POST['videoLink'])) {
-    $checked[] = (isset($autoRedirect))? 'checked' : ' ';
-    $checked[] = (isset($redirectSsl))? 'checked' : ' ';
-    $checked[] = (isset($redirectImmediately))? 'checked' : ' ';
-    setcookie("form_default", json_encode($checked), time()+360);
-} else {
-    if(isset($_COOKIE["form_default"])) {
-
-        $checked = json_decode($_COOKIE["form_default"], true);
+function getVideoCaption($conn, $youtube_id) {
+    $query = "SELECT Caption FROM ourtube.video WHERE YoutubeID = '".$youtube_id."'";
+    //echo $query;
+    $result = $conn->query($query);
+    if(!$result) {
+        die("Query failed: ". mysqli_error($conn));
+    }
+    if($row = $result->fetch_assoc()) {
+        return $row['Caption'];
     } else {
-        $autoRedirect = true;
-        $redirectSsl = null;
-        $redirectImmediately = null;
-        $checked[] = (isset($autoRedirect))? 'checked' : ' ';
-        $checked[] = (isset($redirectSsl))? 'checked' : ' ';
-        $checked[] = (isset($redirectImmediately))? 'checked' : ' ';
-        setcookie("form_default", json_encode($checked), time()+360);
+        return -1;
     }
 }
 
-$htmlBody = <<<END
-
-<div class="container">
-  <div class="row">
-    <div class="col-12">
-      <form method="POST" enctype="multipart/form-data">
-        <div class="form-group row">
-          <div class="input-group mb-3">
-            <div class="input-group-prepend">
-              <span class="input-group-text" id="inputGroup-sizing-default">Youtube Video Link</span>
-            </div>
-            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="videoLink" name="videoLink" placeholder="Enter Youtube Video Link" required>
-            <div class="input-group-append">
-              <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Go</button>
-            </div>
-          </div>
-        </div>
-        <div class="form-group row">
-          <div class="col-sm-2">Options</div>
-          <div class="col-sm-10">
-            <div class="form-check">
-              <input class="form-check-input" name="autoRedirect" type="checkbox" value="Yes" id="autoRedirectCheck" $checked[0]>
-              <label class="form-check-label" for="gridCheck1">
-              Auto Redirect
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" name="redirectSSL" type="checkbox" value="Yes" id="redirectSslCheck" disabled $checked[1]>
-              <label class="form-check-label" for="gridCheck1">
-              Redirect Using Secure Mode<b>(Recomanded)</b>
-              </label>
-              <small id="passwordHelpBlock" class="form-text text-muted">
-                Redirect using "https://" protocol.
-              </small>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" name="redirectImmediately" value="Yes" type="checkbox" id="redirectImmediatelyCheck" $checked[2]>
-              <label class="form-check-label" for="gridCheck1">
-              Redirect Immediately
-              </label>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-END;
-
-// service account secrets
-$KEY_FILE_LOCATION = "./awp-hw-c6644f253e84.json";
-
-
-$client = new Google_Client();
-$client->setApplicationName("AWP_HW");
-$client->setAuthConfig($KEY_FILE_LOCATION);
-$client->setScopes(['https://www.googleapis.com/auth/youtube.force-ssl']);
-$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
-FILTER_SANITIZE_URL);
-$client->setRedirectUri($redirect);
-
-// prevent the server stores another token with different scope
-// use the scopes as a session key to store/get the token
-$tokenSessionKey = 'token-' . $client->prepareScopes();
-
-// check if the user sent the data or not
-if(isset($_POST['videoLink'])) {
-    // if so, prepareing for the authentication routine
-    
-    // check if we have the token or not
-    if (isset($_SESSION[$tokenSessionKey])) {
-        // if we've already acquired the authentication token, 
-        // just use it. 
-        $client->setAccessToken($_SESSION[$tokenSessionKey]);
+function getVideoTitle($conn, $youtube_id) {
+  $query = "SELECT Title FROM ourtube.video WHERE YoutubeID = '".$youtube_id."'";
+  $result = $conn->query($query);
+    if(!$result) {
+        die("Query failed: ". mysqli_error($conn));
+    }
+    if($row = $result->fetch_assoc()) {
+        return $row['Title'];
     } else {
-        // if we havn't acquired the access token, 
-        // calling Google Api Library to get it.
-        $client->fetchAccessTokenWithAssertion();
-        $_SESSION[$tokenSessionKey] = $client->getAccessToken();
-        $htmlBody .= '<p> token isn\'t set, using service account to ask access token<br>';
-        $htmlBody .= '<p> and got token = '.$_SESSION[$tokenSessionKey]['access_token'].'<br>';
+        return -1;
     }
+}
 
-    // Check to ensure that the access token was successfully acquired.
-    if ($client->getAccessToken()) {
-        $youtube = new Google_Service_Youtube($client);
-        $videoLink = isset($_POST['videoLink']) ? $_POST['videoLink'] : null;
-        $query = parse_url($videoLink, PHP_URL_QUERY);
-        parse_str($query, $queryResult);
-        
-        $videoId = isset($queryResult['v']) ? $queryResult['v'] : null;
-        try {
-            $captions = listCaptions($youtube, $videoId, $htmlBody);
-            $htmlBody .= '<ul>';
-            foreach ($captions as $line) {
-                if(preg_match('/^en/', $line['snippet']['language'])) {
-                    $englighCaptionId = $line['id'];
-                    //echo $englighCaptionId.'</br>';
-                }
-                $htmlBody .= sprintf('<li>%s(%s) in %s language</li>', $line['snippet']['name'],
-                    $line['id'],  $line['snippet']['language']);
-            }
-            $htmlBody .= '</ul>';
-            if(isset($englighCaptionId)) {
-                $responseHeader = '';
-                $_SESSION[$videoId] = downloadCaption($youtube, $englighCaptionId, $responseHeader);
-                if(isset($redirectSsl)) {
-                    $protocol = 'https://';
-                } else {
-                    $protocol = 'http://';
-                }
-                $redirect_uri = $protocol . $_SERVER['HTTP_HOST'] . '/awp_hw/video.php?youtubeid=' . $videoId . '&use_session=True';
-                $htmlBody .= sprintf("<a href=\"%s\">Go </a>", $redirect_uri);
-                //header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-                if(isset($autoRedirect)) {
-                    $htmlBody .= sprintf('<div id="counter" countDown="6" redirectUri="%s"></div>',
-                    $redirect_uri);  
-                    if(isset($redirectImmediately)) {
-                        header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));  
-                    }
-                }
-                $htmlBody .= $responseHeader;
-            } else {
-                $htmlBody .= "<h3>This video does not have an english caption</h3>";
-            }
-        } catch (Google_Service_Exception $e) {
-            $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
-                'code: '.$e->getCode().'\n'. htmlspecialchars($e->getMessage()));
-            $logout_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/awp_hw/logout.php';        
+function getVideoCount($conn) {
+    $query = 'SELECT COUNT(YoutubeID) as video_num FROM ourtube.video';
+    $result = $conn->query($query);
+    if(!$result) {
+        die("Query failed: ". mysqli_error($conn));
+    }
+    if($count = $result->fetch_assoc()) {
+        return $count['video_num'];
+    } else {
+        return -1;
+    }
+}
 
-            $htmlBody .= sprintf('<p>Maybe your authentication is out of date, we have refresh the authentication, please </p><a href="%s">try again</a></p>', filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
-FILTER_SANITIZE_URL));
-        } catch (Google_Exception $e) {
-            $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
-            htmlspecialchars($e->getMessage()));
+
+function getVideoList($conn, $page, $size) { 
+    global $database_thumbnail_folder_path, $host;
+    if($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $query = 'SELECT YoutubeID, VoicetubeID, Title FROM ourtube.video LIMIT '.$page*$size.','.$size;
+    $result = $conn->query($query);
+    
+    if(!$result) {
+        die("Query failed: ". mysqli_error($conn));
+    }
+    $json_str = '[
+        ';
+    $first = 1;
+    while($row = $result->fetch_assoc()){
+        $thumbnail_link = $host.$database_thumbnail_folder_path.$row['VoicetubeID'].'/'.$row['YoutubeID'].'.jpg';
+        // $thumbnail_link = glob($host.$database_thumbnail_folder_path.$row['VoicetubeID'].'/*.jpg');
+        if(!$first) {
+            $json_str .= ',';
         }
+        $json_str .= '{
+            "videoID":"'.$row['YoutubeID'].'",
+            "thumbnail_link":"'.$thumbnail_link.'",
+            "title":"'.$row['Title'].'"
+        }';
+        // $json_str .= '{
+        //     "videoID":"'.$row['YoutubeID'].'",
+        //     "thumbnail_link":"'.$thumbnail_link[0].'",
+        //     "title":"'.$row['Title'].'"
+        // }';
+        $first = 0;
     }
+
+    $json_str .= '
+]';
+    return $json_str;
+}
+
+function getVideoSearchList($conn, $q, $page, $size) { 
+    global $database_thumbnail_folder_path, $host;
+    if($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $query = 'SELECT YoutubeID, VoicetubeID, Title FROM ourtube.video WHERE Title LIKE '.$q.' LIMIT '.$page*$size.','.$size;
+    //echo $query;
+    $result = $conn->query($query);
+    
+    if(!$result) {
+        die("Query failed: ". mysqli_error($conn));
+    }
+    $query2 = 'SELECT Count(YoutubeID) as total FROM ourtube.video WHERE Title LIKE '.$q;
+
+    $total_result = $conn->query($query2);
+    
+    if(!$total_result) {
+        die("Query failed: ". mysqli_error($conn));
+    }
+    $json_str = '[
+        ';
+    $first = 1;
+    while($row = $result->fetch_assoc()){
+        $thumbnail_link = $host.$database_thumbnail_folder_path.$row['VoicetubeID'].'/'.$row['YoutubeID'].'.jpg';
+        // $thumbnail_link = glob($host.$database_thumbnail_folder_path.$row['VoicetubeID'].'/*.jpg');
+        if(!$first) {
+            $json_str .= ',';
+        }
+        $json_str .= '{
+            "videoID":"'.$row['YoutubeID'].'",
+            "thumbnail_link":"'.$thumbnail_link.'",
+            "title":"'.$row['Title'].'"
+        }';
+        // $json_str .= '{
+        //     "videoID":"'.$row['YoutubeID'].'",
+        //     "thumbnail_link":"'.$thumbnail_link[0].'",
+        //     "title":"'.$row['Title'].'"
+        // }';
+        $first = 0;
+    }
+    
+    if($num = $total_result->fetch_assoc()) {
+        $total_num = $num['total'];
+    }
+    
+    $json_str .= '
+]';
+    return Array($json_str, $total_num);
 }
 
 /**
- * Returns a list of caption tracks. (captions.listCaptions)
+ * Simple helper to debug to the console
  *
- * @param Google_Service_YouTube $youtube YouTube service object.
- * @param string $videoId The videoId parameter instructs the API to return the
- * caption tracks for the video specified by the video id.
- * @param $htmlBody - html body.
+ * @param $data object, array, string $data
+ * @param $context string  Optional a description.
+ *
+ * @return string
  */
-function listCaptions(Google_Service_YouTube $youtube, $videoId, &$htmlBody) {
-  // Call the YouTube Data API's captions.list method to retrieve video caption tracks.
-  $captions = $youtube->captions->listCaptions("snippet", $videoId);
+function debug_to_console( $data, $context = 'Debug in Console' ) {
 
-  return $captions;
+    // Buffering to solve problems frameworks, like header() in this and not a solid return.
+    ob_start();
+
+    $output  = 'console.info( \'' . $context . ':\' );';
+    $output .= 'console.log(' . json_encode( $data ) . ');';
+    $output  = sprintf( '<script>%s</script>', $output );
+
+    echo $output;
 }
 
+// Database insert. Insert caption info to the Caption table
+function insertVideoCaption(
+	$captionId,	// The caption id from Youtube
+	$videoId,	// The video id from Youtube
+	$sequence,	// The sequence number to the text content 
+	$start,		// The start time of the caption
+	$duration,	// The duration of the caption
+	$content	// The text content of the caption
+){
+	$conn=connectOurtubeDatabase();
+	if($conn->connect_error) {
+		
+        debug_to_console("Connection failed: " . $conn->connect_error);
+		if(!is_null($conn)){
+			mysqli_close($conn);
+		}
+		return;
+    }
+	
+	$sql=sprintf("select * from ourtube.video_caption where videoid='%s' and captionId='%s';"
+	,$videoId,$captionId);
+	$result = $conn->query($sql);
+	
+	if(!$result) {
+		debug_to_console("Query failed: ". mysqli_error($conn));
+		mysqli_close($conn);
+		return; 
+    }
+	
+	$rows=mysqli_num_rows($result);
+	debug_to_console('rows:'.$rows);
+	// Close db connection
+	mysqli_close($conn);
+	
+	$mysqli=new mysqli("40.121.221.31", "nthuuser", "1qaz@WSX3edc","ourtube");	
+	if($mysqli->connect_error){
+		debug_to_console("Connection failed: ".mysqli_error($mysqli));
+		$mysqli->close();
+		return;
+	}
+	
+	// Set autocommit to off
+	//mysqli_autocommit($mysqli,FALSE);
 
+	//$mysqli->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
 
+	
+		$sql= sprintf("Insert ignore into Video_Caption(videoId, captionId) values('%s','%s');",$videoId,$captionId); 
+		$mysqli->query($sql);
+		
+		if($mysqli->query($sql)!==True){
+			debug_to_console("Failed to insert data to Video_Caption!");
+			$mysqli->close();
+			return;
+		}
+		
+		debug_to_console("Succeeded to insert into table video_caption;");
+		
+		$sql=sprintf("insert ignore into Caption(captionId,sequence,start,duration,content)			values('%s',%d,'%s','%s','%s');",$captionId,$sequence,$start,$duration,$content);
+		if($mysqli->query($sql)===True){
+			debug_to_console("Succeeded to insert into table caption;");
+		}else{
+			debug_to_console("Faile to insert into table caption;");
+		}
+	
+		
+	//debug_to_console("sql: ".$sql);	
+	
+	//close the connection;
+	$mysqli->close();		
+}
+
+// Database insert. Insert video info into YoutubeVideo table.
+function insertYoutubeVideio($Video){
+	$mysqli=new mysqli("40.121.221.31", "nthuuser", "1qaz@WSX3edc","ourtube");	
+	if($mysqli->connect_error){
+		debug_to_console("Connection failed: ".$mysqli->connect_error);
+		return;
+	}
+
+	$sql= sprintf("Select * from YoutubeVideo where videoId='%s';",$Video->videoId); 
+	debug_to_console("sql:".$sql);
+	$result=$mysqli->query($sql);
+	if(!$result){
+		debug_to_console("Failed to select the YoutubeVideo!".mysqli_error($mysqli));
+		$mysqli->close();
+		return;
+	}	
+	
+	$rows=mysqli_num_rows($result);
+	debug_to_console("returned rows:".$rows);
+	if($rows==0){
+		$sql= sprintf("insert ignore into YoutubeVideo(videoId,title,description,video_width,video_height,thumbnail_url,thumbnail_width,thumbnail_height,author_url,author_name,channelId,publishDate) values('%s','%s','%s',%d,%d,'%s',%d,%d,'%s','%s','%s','%s');"
+		,$Video->videoId,$Video->title,$Video->description,$Video->video_width,$Video->video_height,$Video->thumbnail_url,$Video->thumbnail_width,$Video->thumbnail_height,$Video->author_url,$Video->author_name,$Video->channelId,$Video->publishDate); 
+		
+		$mysqli->query($sql);
+		if(!$mysqli){
+			debug_to_console("Failed to insert data into YoutubeVideo!");
+			$mysqli->close();
+			return;
+		}else{
+			debug_to_console("Succeeded to insert data into YoutubeVideo!");
+		}
+	}
+	
+	// close the objec
+	$mysqli->close();
+}
+
+// Database Update
+
+// Receive ajax post request
+if (isset($_POST['request'])) {
+    $conn = connectOurtubeDatabase();
+    
+    if ($conn && $_POST['request'] == "getVideoList") {
+        $total_video_num = getVideoCount($conn);
+        if ($_POST['page']*$_POST['size'] > $total_video_num) {
+            $response = "reachedMax";
+        } else {
+            $response = getVideoList($conn, $_POST['page'], $_POST['size']);
+        }
+    }
+    exit($response);
+}
 ?>
-<!doctype html>
-<html>
-<head>
-  <?php echoBootstrapAndJqueryDependencies(); ?>
-  <!-- our own css and js file -->
-  <script src="assets/javascripts/youtube-captions.js"></script>
-  <link rel="stylesheet" type="text/css" href="assets/css/home.css"/>   
-<title>Create and manage video caption tracks</title>
-</head>
-<body>
-  <?=$htmlBody?>
-</body>
-</html>
